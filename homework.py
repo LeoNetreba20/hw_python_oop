3ffe50809f8fc235b4ca7eff6b65fadfb0a40999
LeoNetreba20 <81775876+LeoNetreba20@users.noreply.github.com>
2021-04-12 19:57:10 +0400

    Исправил ошибка
    Исправил ошибка

--- homework.py
+++ homework.py
@@ -29,7 +29,7 @@ class Calculator:
             # Эта функция(метод) отвечает за то что бы записывать
             # все затраты сегодня, так что тут мы берём только те записи
             # которые с сегодняшней датой
-            if record.date == dt.datetime.now().date():
+            if record.date == dt.date.today():
                 # И чтобы было удобно и красиво ложем в пустую переменною
                 total = total + record.amount
 
@@ -49,6 +49,10 @@ class Calculator:
 
         return total
 
+    # Отнимаем от всего лимита потраченное за день
+    def get_balance_money(self):
+        return self.limit - self.get_today_stats()
+
 
 # Тут говорится о балансе, что осталось или наоборот
 class CashCalculator(Calculator):
@@ -60,42 +64,36 @@ class CashCalculator(Calculator):
     # Сurrency это курс в котором хочет его увидеть пользователь,
     # доступные значения - 'rub', 'eur', 'usd'.
     def get_today_cash_remained(self, currency):
-        # Отнимаем от всего лимита потраченное за день
-        ostatok = self.limit - self.get_today_stats()
-
         courses = {
             'rub': ('руб', self.RUB_RATE),
             'usd': ('USD', self.USD_RATE),
             'eur': ('Euro', self.EURO_RATE)
         }
 
-        # Здесь приводи его к вибронному курсу
-        result = ostatok / courses[currency][1]
+        # Здесь приводи остаток к вибронному курсу
+        result = self.get_balance_money() / courses[currency][1]
 
         # И переделываем из такого 3,293921039 в эта 3,92 - обрезаем :)
         result = round(result, 2)
 
-        # Если остаток равняется 0,
-        # приводить его в полноценное число - 555.0 = 555
-        if (result - int(result)) == 0.0:
-            result = int(result)
-
         # Сюда ложем названия курса
         course_name = courses[currency][0]
 
-        # Если осталось больше 0
-        if result > 0:
-            return f'На сегодня осталось {result} {course_name}'
-
-        # Если превышен лимит
-        elif result < 0:
-            # Функция abs убирает минусы с отрицательных чисел
-            return ('Денег нет, держись: '
-                    f'твой долг - {abs(result)} {course_name}')
-
-        # И если равен 0
-        else:
+        # Если равен 0
+        if result == 0:
             return 'Денег нет, держись'
+
+        # Если не равен 0
+        else:
+            # Если осталось больше 0
+            if result > 0:
+                return f'На сегодня осталось {result} {course_name}'
+
+            # Если превышен лимит
+            elif result < 0:
+                # Функция abs убирает минусы с отрицательных чисел
+                return ('Денег нет, держись: '
+                        f'твой долг - {abs(result)} {course_name}')
 
 
 # Тут о том сколько нужна еще скушать или хватит есть)
@@ -103,7 +101,7 @@ class CaloriesCalculator(Calculator):
     # Тут практически все то же самое как в классе CashCalculator только
     # без всяких курсов и выводится все про калории
     def get_calories_remained(self):
-        ostatok = self.limit - self.get_today_stats()
+        ostatok = self.get_balance_money()
 
         if ostatok > 0:
             return (f'Сегодня можно съесть что-нибудь ещё, но с общей '
@@ -120,15 +118,33 @@ class Record:
     date_format = '%d.%m.%Y'
 
     # Вот тут свойства, что бы потом можно было класть в них данные
-    def __init__(self, amount, comment, date=''):
+    def __init__(self, amount, comment, date=None):
         self.amount = amount
         self.comment = comment
 
         # Здесь говорится что нужно если в date ничего не укажут ставить
         # сегодняшнею дату
-        if date == '':
-            self.date = dt.datetime.now().date()
+        if date is None:
+            self.date = dt.date.today()
 
         # Тут если указана дата, приводим её к нужному формату
         else:
             self.date = dt.datetime.strptime(date, self.date_format).date()
+
+
+cash_calculator = CashCalculator(1000)
+
+# дата в параметрах не указана,
+# так что по умолчанию к записи
+# должна автоматически добавиться сегодняшняя дата
+cash_calculator.add_record(Record(amount=145, comment='кофе'))
+# и к этой записи тоже дата должна добавиться автоматически
+cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
+# а тут пользователь указал дату, сохраняем её
+cash_calculator.add_record(Record(amount=3000,
+                                  comment='бар в Танин др',
+                                  date='08.11.2019'))
+
+print(cash_calculator.get_today_cash_remained('eur'))
+# должно напечататься
+# На сегодня осталось 555.0 руб
